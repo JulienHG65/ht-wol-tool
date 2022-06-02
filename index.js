8b18d97ab540ccb6d097859ca9f0d48aeaabca5d
JulienHG65 <julien.onneinCD65@outlook.fr>
2022-06-02 11:32:21 +0200

    Revert "Edit ping feature"
    This reverts commit a49afc2cc453f745ced822d6838da670d07973eb.

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ intervalPing = 30
 //================================================
 
 //======================CODE======================
+const ping = require('ping');
 const wol = require('wol'); 
-const { exec } = require("child_process");
 const express = require('express')
 const app = express()
 function pg(host){//function to ping host
-    exec(`ping -c 1 ${host}`)
+    ping.sys.probe(host);
+
 }
 
 
@@ -28,35 +29,23 @@ setInterval(function(){//ping with interval
 },intervalPing*1000)
 
 
-app.get('/host/:host', function (req, res) {
+app.get('/:host', function (req, res) {
     let r = res
     for(i=0;devices[i];i++){
-        if(req.params.host.toLowerCase()==devices[i].host.toLowerCase()){
+        if(req.params.host.toLowerCase()==devices[i].host){
             wol.wake(devices[i].mac, function(err, res){
-                if(res===true){
-                    console.log(`${devices[i].host} is awake`);
-                }
                 r.send(res)
+                if(res===true){
+                    console.log(`${devices[i].host} a bien été réveillé`);
+                }
             });
             break
         }
     }
 })
-app.get('/all', function (req, res) {
-    for(i=0;devices[i];i++){
-        let host = devices[i].host
-    
-	    wol.wake(devices[i].mac, function(err, res){
-            if(res===true){
-                console.log(`${host} is awake`);
-            }
-	    });
-    }
-    res.send(true)
-})
 
 app.listen(port,function(){
-    console.log(`Listen HTTP port is ${port}`)
-    console.log(`Ping all devices every ${intervalPing} secondes`)
+    console.log(`Le port d'écoute HTTP est le ${port}`)
+    console.log(`Un ping de l'ensemble des postes sera fait toutes les ${intervalPing} secondes`)
 
 })
